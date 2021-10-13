dfc171e7c58e3b987bf4c4b07c94064884ebd3a1
PauloConsoni <paulo.consoni4000@gmail.com>
2021-10-13 12:38:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tgiot/ui/activity/FormularioThingActivity.java
@@ -0,0 +1,16 @@
+package com.example.tgiot.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tgiot.R;
+
+public class FormularioThingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_thing);
+    }
+}
