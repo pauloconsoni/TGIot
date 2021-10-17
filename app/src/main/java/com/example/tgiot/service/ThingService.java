182d277e3db15d65e62aadbb6c2aa8506bbe69a8
PauloConsoni <paulo.consoni4000@gmail.com>
2021-10-16 23:51:24 -0300

    mudando requisição para query string

--- app/src/main/java/com/example/tgiot/service/ThingService.java
+++ app/src/main/java/com/example/tgiot/service/ThingService.java
@@ -5,9 +5,10 @@ import com.example.tgiot.model.RequestResponseTotal;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ThingService {
 
-    @GET("prod/{url}")
-    Call<RequestResponseTotal> buscaDados(@Path("url") String url);
+    @GET("test/iottg")
+    Call<RequestResponseTotal> buscaDados(@Query("thing_id") String thingId);
 }
